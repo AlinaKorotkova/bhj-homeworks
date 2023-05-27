1b94db1cb57d5cb9076bf1069211ca46f25711d1
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-05-27 21:43:19 +0300

    Add files via upload

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,15 @@
+const cookie = document.getElementById("cookie");
+
+const clicker__counter = document.getElementById("clicker__counter");
+let count = 0;
+
+cookie.onclick = function () {
+  if (count % 2 === 0) {
+    cookie.width = 100;
+  } else {
+    cookie.width = 300;
+  }
+  
+  count++;
+  clicker__counter.textContent = count;
+ };
