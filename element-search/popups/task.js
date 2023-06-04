2e55e164a3ffb507f1934b232a949d4c5396b7cb
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-06-04 16:24:13 +0300

    Add files via upload

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+modalMain = document.getElementById('modal_main');
+modalClose = document.getElementsByClassName('modal__close');
+modalSuccess = document.getElementById('modal_success');
+
+
+modalMain.classList.add('modal_active');
+
+
+modalClose[0].onclick = function () { //Закрыть modal_main
+    modalMain.classList.remove('modal_active');
+}
+
+modalClose[2].onclick = function () { //Закрыть modal_success
+    modalSuccess.classList.remove('modal_active');
+}
+
+
+modalClose[1].onclick = function () {  //Открытие modal_success
+    modalMain.classList.remove("modal_active");
+    modalSuccess.classList.add("modal_active");
+}
