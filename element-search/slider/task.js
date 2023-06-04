b131908cccdd41c2ee780fef01ca45c4c72605eb
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-06-04 18:55:48 +0300

    Add files via upload

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,13 +1,19 @@
-const clickEl = document.querySelectorAll(".menu__link");
+const arrSlider = Array.from(document.getElementsByClassName("slider__item"));
 
-clickEl.forEach((item) => {
-  item.addEventListener("click", (event) => {
-    const li = event.target.closest("li");
-    const ul = li.querySelector("ul");
-    console.log(ul);
-    if (ul) {
-      event.preventDefault();
-      ul.classList.toggle("menu_active");
-    }
-  });
-});
+function slide(activeSlideIndex) {
+	let activeSlide = arrSlider.findIndex(a => a.className.includes("slider__item_active"));
+	if (activeSlideIndex < 0) activeSlideIndex = arrSlider.length - 1;
+	if (activeSlideIndex > arrSlider.length - 1) activeSlideIndex = 0;
+	arrSlider[activeSlide].className = "slider__item";
+	arrSlider[activeSlideIndex].className += " slider__item_active";
+}
+
+document.querySelector(".slider__arrow_next").onclick = function () {
+	let idx = arrSlider.findIndex(a => a.className.includes("slider__item_active"));
+	slide(idx + 1);
+}
+
+document.querySelector(".slider__arrow_prev").onclick = function () {
+	let idx = arrSlider.findIndex(a => a.className.includes("slider__item_active"));
+	slide(idx - 1);
+}
