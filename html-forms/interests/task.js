df2c0e73853be2b8466494320bacef57ca74e064
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-07-30 22:02:15 +0300

    Add files via upload

---
+++ html-forms/interests/task.js
@@ -0,0 +1,13 @@
+'use strict'
+
+window.onload = function() {
+	let list = Array.from(document.querySelectorAll('div.interests_main > ul > li > label > input'));
+	for (let element of list) {
+		element.onchange = function() {
+			let nestedList = Array.from(element.parentNode.parentNode.querySelectorAll('input'));
+			for (let el of nestedList) {
+				el.checked = element.checked;
+			}
+		};
+	}
+};
