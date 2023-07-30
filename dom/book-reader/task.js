ca7cb87780628b54277087fde1ef7f364d61177f
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-07-30 17:47:46 +0300

    Add files via upload

---
+++ dom/book-reader/task.js
@@ -0,0 +1,12 @@
+const buttonFontSize = Array.from(document.getElementsByClassName('font-size'));
+const bookContent = document.querySelector('.book__content');
+
+buttonFontSize.forEach( element => {
+	element.onclick = function (e) {
+		buttonFontSize.forEach(element => 
+			element.className = `font-size font-size_${element.dataset.size}`);
+		e.currentTarget.className = `font-size font-size_${element.dataset.size} font-size_active`
+		bookContent.className = 'book__content font-size_' + element.dataset.size;
+		e.preventDefault();
+	}
+})
